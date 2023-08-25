4c46d3e86caf9b4acdd95556aa4b86390b54335d
Arya Putra Kusumah <117886823+AryaKusumah2004@users.noreply.github.com>
2023-08-25 22:27:02 +0700

    Add files via upload

---
+++ Tugas 1/Welcome.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * @author Arya Putra Kusumah
+ */
+public class Welcome {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String greeting = "Welcome to Core Java!";
+        System.out.println(greeting);    
+        for (int i = 0; i < greeting.length(); i++)
+            System.out.print("=");
+        System.out.println();
+    }
+    
+}
